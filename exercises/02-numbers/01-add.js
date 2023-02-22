1c1b233b18dc6f00ff43785da049124f22d37299
Kevin Browne <kbrowne97@gmail.com>
2023-02-22 18:59:11 -0500

    complete: numbers exercises

--- exercises/02-numbers/01-add.js
+++ exercises/02-numbers/01-add.js
@@ -2,5 +2,6 @@
  * Create a constant called "sum".
  * On the right side of the "equals" sign, there should two numbers and a plus sign.
  */
-
 // WRITE YOUR ANSWER BELOW THIS LINE
+
+const sum = 2 + 2;
