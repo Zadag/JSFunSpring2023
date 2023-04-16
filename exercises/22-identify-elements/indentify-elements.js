68d98ac13c886afe5d50bbbe0854e9b2d4ad36d4
Jamal Taylor <jtaylor@advisorengine.com>
2023-04-16 14:55:24 -0400

    [class-4-20] added assignments and changed the structure of one assignment

--- exercises/22-identify-elements/indentify-elements.js
+++ exercises/22-identify-elements/indentify-elements.js
@@ -1,4 +1,4 @@
-!(function () {
+(function () {
   /**
    * When the user clicks on one of the "Select" buttons to select a plan,
    * display the following message in the aqua green notification at the top of the screen,
